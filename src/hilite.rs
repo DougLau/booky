63f18b989bd2622b7ff5c22751dd04386ed380dc
Doug Lau <doug.p.lau@gmail.com>
2025-05-18 13:22:48 -0500

    Improved highlighting

--- src/hilite.rs
+++ src/hilite.rs
@@ -1,7 +1,9 @@
 use crate::kind::Kind;
+use crate::lex;
 use crate::parse::Parser;
+use crate::word::WordClass;
 use std::io::BufRead;
-use yansi::Paint;
+use yansi::{Paint, Style};
 
 /// Hilite text from a reader
 pub fn hilite_text<R>(reader: R) -> Result<(), std::io::Error>
@@ -10,12 +12,36 @@ where
 {
     for chunk in Parser::new(reader) {
         let (_chunk, text, kind) = chunk?;
-        if kind == Kind::Unknown {
-            print!("{}", text.underline());
-        } else {
-            print!("{text}");
-        }
+        print!("{}", text.paint(style(kind, &text)));
     }
     println!();
     Ok(())
+}
+
+/// Get style to paint a chunk
+fn style(kind: Kind, word: &str) -> Style {
+    match kind {
+        Kind::Lexicon => {
+            let mut ents = lex::builtin().word_entries(word);
+            if ents.len() != 1 {
+                return Style::new();
+            }
+            let word = ents.pop().unwrap();
+            match word.word_class() {
+                WordClass::Noun => Style::new().bright_red(),
+                WordClass::Pronoun => Style::new().red(),
+                WordClass::Verb => Style::new().bright_green(),
+                WordClass::Adverb => Style::new().bright_cyan(),
+                _ => Style::new().bright_white(),
+            }
+        }
+        Kind::Foreign => Style::new().bright().italic(),
+        Kind::Ordinal | Kind::Roman | Kind::Number => {
+            Style::new().bright_blue()
+        }
+        Kind::Proper => Style::new().bold(),
+        Kind::Symbol => Style::new().dim(),
+        Kind::Unknown => Style::new().underline(),
+        _ => Style::new(),
+    }
 }
